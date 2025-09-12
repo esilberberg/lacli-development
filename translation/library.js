db8a6477de77ad83a67725611fc345846cf557c8
esilberberg <ericsil218@gmail.com>
2025-09-12 13:06:19 -0400

    Esp translation of search help modal on library

--- translation/library.js
+++ translation/library.js
@@ -5,10 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const randomBtn = document.getElementById('random-btn-text');
     const exportBtn = document.getElementById('export-btn-text');
     const filtersBtn = document.getElementById('filters-btn-text');
+    const modal = document.getElementById('modal-content-text');
 
-
-
-  
     const storedLanguage = localStorage.getItem('lacliLanguagePreference');
   
     if (storedLanguage) {
@@ -37,6 +35,24 @@ document.addEventListener('DOMContentLoaded', function () {
           random: 'Random',
           export: 'Download',
           filters: 'Filters',
+          modal: `<div class="modal-heading">About LACLI Search</div>
+              <h3><i class="fa-solid fa-magnifying-glass"></i> Keywords</h3>
+              <ul>
+                  <li>Searches are not case sensitive and ignore diacritics.</li>
+                  <li>Truncation, wildcards, quotation marks, and lemmatization are not supported.</li>
+                  <li>It is preferable to search with singular rather than plural nouns.</li>
+                  <li>Search terms separated by a space are treated as independent search terms and are linked with the AND operator. Therefore, all search terms must appear in at least one description field for a resource to be returned in the results.</li>
+              </ul>
+              <h3><i class="fa-solid fa-sliders"></i> Filters</h3>
+              <ul>
+                <li>When a user selects multiple values from the same filter, such as two languages, LACLI uses OR logic. This means it shows results that match at least one of the selected values (e.g., Spanish OR English).</li>
+                <li>When a user selects values from different filters, like a format and a language, LACLI uses AND logic. This narrows the results to only those that match all of the selected criteria (e.g., Books AND Spanish).</li>
+              </ul>
+              <h3><i class="fa-solid fa-floppy-disk"></i> Saving</h3>
+              <ul>
+                <li>There are two ways to save a search. First, copy the url. Only keywords, and not filters, are captured in the url. Second, download the records currently displayed as a JSON file to your computer via the Download button.</li>
+              </ul>
+            </div>`
         },
         es: {
           placeholder: 'Términos de búsqueda',
@@ -44,7 +60,24 @@ document.addEventListener('DOMContentLoaded', function () {
           random: 'Aleatorio',
           export: 'Descargar',
           filters: 'Filtros',
-
+          modal: `<div class="modal-heading">Acerca de la búsqueda</div>
+              <h3><i class="fa-solid fa-magnifying-glass"></i> Palabras clave</h3>
+              <ul>
+                  <li>Las búsquedas ignoran los diacríticos y no distinguen entre mayúsculas y minúsculas.</li>
+                  <li>No están implementadas funciones como la búsqueda con truncamiento, los comodines, el uso de comillas, o la lematización.</li>
+                  <li>Es preferible buscar con sustantivos en singular que en plural.</li>
+                  <li>Los términos de búsqueda separados por un espacio se consideran términos de búsqueda independientes y se vinculan con el operador AND. Por lo tanto, todos los términos de búsqueda deben aparecer en al menos un campo de descripción para que un recurso aparezca en los resultados.</li>
+              </ul>
+              <h3><i class="fa-solid fa-sliders"></i> Filtros</h3>
+              <ul>
+                <li>Cuando un usuario selecciona varios valores del mismo filtro, como dos idiomas, LACLI utiliza el operador OR. Esto significa que muestra resultados que coinciden con al menos uno de los valores seleccionados (p. ej., español OR inglés).</li>
+                <li>Cuando un usuario selecciona valores de diferentes filtros, como un formato y un idioma, LACLI utiliza el operador AND. Esto limita los resultados a solo aquellos que coinciden con todos los criterios seleccionados (p. ej., libros AND español).</li>
+              </ul>
+              <h3><i class="fa-solid fa-floppy-disk"></i> Guardar</h3>
+              <ul>
+                <li>Hay dos maneras de guardar una búsqueda: 1) Copie la URL. Tenga en cuenta que en la URL solo se conservan las palabras clave, no los filtros. 2) Descargue los registros que se muestran actualmente como archivo JSON a su computadora mediante el botón Descargar.</li>
+              </ul>
+            </div>`
         },
         pt: {
           placeholder: 'Termos pesquisados',
@@ -52,6 +85,24 @@ document.addEventListener('DOMContentLoaded', function () {
           random: 'Aleatório',
           export: 'Exportar',
           filters: 'Filtros',
+          modal: `<div class="modal-heading">Acerca de la búsqueda</div>
+              <h3><i class="fa-solid fa-magnifying-glass"></i> Palabras clave</h3>
+              <ul>
+                  <li>Las búsquedas ignoran los diacríticos y no distinguen entre mayúsculas y minúsculas.</li>
+                  <li>No están implementadas funciones como la búsqueda con truncamiento, los comodines, el uso de comillas, o la lematización.</li>
+                  <li>Es preferible buscar con sustantivos en singular que en plural.</li>
+                  <li>Los términos de búsqueda separados por un espacio se consideran términos de búsqueda independientes y se vinculan con el operador AND. Por lo tanto, todos los términos de búsqueda deben aparecer en al menos un campo de descripción para que un recurso aparezca en los resultados.</li>
+              </ul>
+              <h3><i class="fa-solid fa-sliders"></i> Filtros</h3>
+              <ul>
+                <li>Cuando un usuario selecciona varios valores del mismo filtro, como dos idiomas, LACLI utiliza el operador OR. Esto significa que muestra resultados que coinciden con al menos uno de los valores seleccionados (p. ej., español OR inglés).</li>
+                <li>Cuando un usuario selecciona valores de diferentes filtros, como un formato y un idioma, LACLI utiliza el operador AND. Esto limita los resultados a solo aquellos que coinciden con todos los criterios seleccionados (p. ej., libros AND español).</li>
+              </ul>
+              <h3><i class="fa-solid fa-floppy-disk"></i> Guardar</h3>
+              <ul>
+                <li>Hay dos maneras de guardar una búsqueda: 1) Copie la URL. Tenga en cuenta que en la URL solo se conservan las palabras clave, no los filtros. 2) Descargue los registros que se muestran actualmente como archivo JSON a su computadora mediante el botón Descargar.</li>
+              </ul>
+            </div>`
         },
       };
   
@@ -60,6 +111,7 @@ document.addEventListener('DOMContentLoaded', function () {
       randomBtn.textContent = translations[language].random;
       exportBtn.textContent = translations[language].export;
       filtersBtn.textContent = translations[language].filters;
+      modal.innerHTML = translations[language].modal;
 
     }
   
