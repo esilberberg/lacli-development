7b9bcd0ac2190893077ba43b4afee6c153fc6ee4
esilberberg <ericsil218@gmail.com>
2025-09-22 12:35:20 -0400

    port trans to library

--- translation/library.js
+++ translation/library.js
@@ -85,22 +85,22 @@ document.addEventListener('DOMContentLoaded', function () {
           random: 'Aleatório',
           export: 'Exportar',
           filters: 'Filtros',
-          modal: `<div class="modal-heading">Acerca de la búsqueda</div>
-              <h3><i class="fa-solid fa-magnifying-glass"></i> Palabras clave</h3>
+          modal: `<div class="modal-heading">Sobre a Busca</div>
+              <h3><i class="fa-solid fa-magnifying-glass"></i> Palavras-chave</h3>
               <ul>
-                  <li>Las búsquedas ignoran los diacríticos y no distinguen entre mayúsculas y minúsculas.</li>
-                  <li>No están implementadas funciones como la búsqueda con truncamiento, los comodines, el uso de comillas, o la lematización.</li>
-                  <li>Es preferible buscar con sustantivos en singular que en plural.</li>
-                  <li>Los términos de búsqueda separados por un espacio se consideran términos de búsqueda independientes y se vinculan con el operador AND. Por lo tanto, todos los términos de búsqueda deben aparecer en al menos un campo de descripción para que un recurso aparezca en los resultados.</li>
+                  <li>As buscas não diferenciam maiúsculas de minúsculas e ignoram os diacríticos (como acentos agudo, circunflexo e grave; o til, a cedilha e o trema).</li>
+                  <li>Truncamento, curingas, aspas e lematização não são suportados pela pesquisa.</li>
+                  <li>Prefira pesquisar com palavras no singular em vez de no plural.</li>
+                  <li>Os termos de pesquisa separados por um espaço são tratados como termos de pesquisa independentes e são ligados pelo operador AND. Portanto, todos os termos de pesquisa devem aparecer em pelo menos um campo de descrição para que um recurso seja retornado nos resultados.</li>
               </ul>
               <h3><i class="fa-solid fa-sliders"></i> Filtros</h3>
               <ul>
-                <li>Cuando un usuario selecciona varios valores del mismo filtro, como dos idiomas, LACLI utiliza el operador OR. Esto significa que muestra resultados que coinciden con al menos uno de los valores seleccionados (p. ej., español OR inglés).</li>
-                <li>Cuando un usuario selecciona valores de diferentes filtros, como un formato y un idioma, LACLI utiliza el operador AND. Esto limita los resultados a solo aquellos que coinciden con todos los criterios seleccionados (p. ej., libros AND español).</li>
+                <li>Quando um usuário seleciona várias opções de um mesmo filtro, como dois idiomas, por exemplo, o LACLI usa o operador booleano OR. Isso significa que ele mostra resultados que correspondem a pelo menos uma das opções selecionadas (por exemplo, Espanhol OU Inglês).</li>
+                <li>Quando um usuário seleciona opções de filtros diferentes, como um formato e um idioma, o LACLI usa o operador AND. Isso restringe os resultados apenas àqueles que correspondem a todos os critérios selecionados (por exemplo, Livros AND Espanhol).</li>
               </ul>
-              <h3><i class="fa-solid fa-floppy-disk"></i> Guardar</h3>
+              <h3><i class="fa-solid fa-floppy-disk"></i> Salvando os resultados</h3>
               <ul>
-                <li>Hay dos maneras de guardar una búsqueda: 1) Copie la URL. Tenga en cuenta que en la URL solo se conservan las palabras clave, no los filtros. 2) Descargue los registros que se muestran actualmente como archivo JSON a su computadora mediante el botón Descargar.</li>
+                <li>Existem duas maneiras de salvar uma pesquisa. Primeiro, copie a URL. Apenas as palavras-chave, e não os filtros, são capturadas na URL. Segundo, baixe os registros atualmente exibidos para o seu computador através do botão Download (arquivo em formato JSON).</li>
               </ul>
             </div>`
         },
